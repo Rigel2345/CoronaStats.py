b7950d86e7d52c785cca9f2593959b5a0ffe4291
Gogo <67242827+Shlok-C@users.noreply.github.com>
2021-09-25 15:19:21 -0700

    Add files via upload

---
+++ csv.py
@@ -0,0 +1,19 @@
+al_name = "Alabama"
+al_daily_avg = 2806
+al_cases_100k = 57
+al_cases_per_14 = 26
+
+mad_name = "Madison"
+mad_daily_avg = 195
+mad_cases_100k = 52
+mad_cases_per_14k = 19
+
+jack_name = "Jackson"
+jack_daily_avg = 39
+jack_cases_100k = 75
+jack_cases_per_14 = 1
+
+limestone_name = "Limestone"
+limestone_daily_avg = 64
+limestone_cases_100k = 64
+limestone_cases_per_14 = 16
